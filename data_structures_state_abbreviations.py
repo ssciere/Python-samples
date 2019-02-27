d3170676079cf8e7e58b9a6b458a13df501120ac
ssciere <40736682+ssciere@users.noreply.github.com>
2019-02-27 11:16:31 -0500

    Update data_structures_state_abbreviations.py

--- data_structures_state_abbreviations.py
+++ data_structures_state_abbreviations.py
@@ -4,6 +4,9 @@ Python to Me" Podcast
 
 To use this program, you will also need to load the states_data.py file from this same repo in the same folder as the program
 
+Instructions for problem:
+Create a script that imports the US States data structures contained in the following script file in our Repo: https://github.com/talkpython/100daysofcode-with-python-course/blob/master/days/07-09-data-structures/code/data.py
+
 Perform the following tasks on the list and dict. The less you look at them, the better this exercise will be. Remember: Dicts are unsorted.
 
 Print out the 10th item in each.
